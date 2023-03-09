b5b5a0e0c6058d6b684a62fbe62757bad852e5f7
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-09 17:39:14 +0300

    .

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -46,7 +46,6 @@ export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<Aff
     // need to fix
     // отбрасывай при помощи метода filter лишних affairs
     return affairs.filter((el) => el._id !== _id)
-    
     //  return filteredTasks // need to fix
 }
 
