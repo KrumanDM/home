b6b55b7f903a195fd6bbd3f7a136a4eb29aaa9b1
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-06-07 22:26:01 +0300

    .

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -22,7 +22,7 @@ function Pages() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 
                 <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR}/>} />
-           
+       
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path="/pre-junior" element={<PreJunior />} />
