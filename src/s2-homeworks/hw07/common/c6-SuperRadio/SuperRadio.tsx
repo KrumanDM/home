79f612d0b9e0e860cca144eee2826190da11c2a1
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-06-09 16:13:36 +0300

    .

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+        if(onChangeOption) {
+            onChangeOption(e.currentTarget.value)
+        } 
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,6 +50,10 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                          
+                      name={"input"}
+                      checked={}
+                      value={value}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
