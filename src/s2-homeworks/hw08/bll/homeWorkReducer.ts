64d3b2e087ec18e1a98f44d3501c4129133316d8
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-06-17 23:27:51 +0300

    .

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
+        case 'sort': { // by name 
+            const newState = [...state];
+            if (action.payload === 'up'){
+                return newState.sort((a, b) => a.name > b.name ? 1 : -1)
+            } else 
+                return newState.sort((a, b) => a.name < b.name ? 1 : -1)// need to fix
+            }
+        
         case 'check': {
-
-            return state // need to fix
+            const newState = [...state];
+            
+            return newState.filter(el => el.age >= 18 )// need to fix
         }
         default:
             return state
