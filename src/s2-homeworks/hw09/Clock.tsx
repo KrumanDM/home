69ed4af6896cf7a993f59d4cc332767f236f90ca
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-07-18 22:35:05 +0300

    .

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -93,7 +93,7 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={(timerId != undefined) ? true : false}// задизэйблить если таймер запущен
+                    disabled={(timerId != undefined) ? true : false}// задизэйблить если таймер запущен, как только запускается таймер он получает timerId
                     onClick={start}  
                 >
                     start
