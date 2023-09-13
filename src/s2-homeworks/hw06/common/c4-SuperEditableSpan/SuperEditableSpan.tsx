ad3ea06adc2feb95fa65fa570e58ed8d80ea910e
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-09-13 22:41:41 +0300

    .

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -91,6 +91,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
 
 
+
                         {children || restProps.value || defaultText}
                     </span>
                 </div>
