e428b8cad6b40864a1374828aa1570ea62f35c1e
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-09-20 19:43:17 +0300

    .

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -98,7 +98,6 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
 export default SuperEditableSpan
  
-
 // let [editMode, setEditMode] = useState(false)
 //     let [title, setTitle] = useState('')
     
