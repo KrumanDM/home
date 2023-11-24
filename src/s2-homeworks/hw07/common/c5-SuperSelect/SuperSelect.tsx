fb23bc9bfb5f928a0f77da89ce293bd32084bc3e
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-11-24 17:08:21 +0300

    refactoring SuperSelect.tsx

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -32,10 +32,9 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
               >
                   {o.value}
               </option>
-
           ))
         : [] // map options with key
-//
+/
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // если onChangeOption вообще существует, то...
         onChange?.(e)
