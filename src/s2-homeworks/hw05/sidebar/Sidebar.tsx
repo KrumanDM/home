84424054bb80bec3d4cd505c5788b040c923160a
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-11-27 18:32:50 +0300

    refactoring Sidebar.tsx

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
             
-            
+        
             <aside className={sidebarClass}>
         
 
